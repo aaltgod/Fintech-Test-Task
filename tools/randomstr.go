365c762c02058910d78d92328bd8033f01986fa1
alyaskastorm <minealaska29@gmail.com>
2021-06-22 20:37:04 +0300

    Add README.md

---
+++ tools/randomstr.go
@@ -0,0 +1,20 @@
+package tools
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomID(IDlen int) string {
+
+	rand.Seed(time.Now().UnixNano())
+
+	alph := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	result := make([]rune, IDlen)
+	for i := range result {
+		result[i] = alph[rand.Intn(len(alph))]
+	}
+
+	return "http://localhost:3000/" + string(result)
+}
--- tools/shortener.go
+++
@@ -1,8 +0,0 @@
-package tools
-
-import "log"
-
-func ShortURL(url string) (string, error) {
-	log.Println("SHORTENER", url+"SHORT")
-	return url+"SHORT", nil
-}
